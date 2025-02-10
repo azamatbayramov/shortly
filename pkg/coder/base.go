09746788e3d3316a0870dac6590f8c950b1dae4a
Azamat Bayramov <bayramov.azamat04@gmail.com>
2025-02-10 13:38:03 +0300

    Add errors to base coder

--- pkg/coder/base.go
+++ pkg/coder/base.go
@@ -2,15 +2,16 @@ package coder
 
 import (
 	"errors"
+	"math"
 	"strings"
 )
 
 type BaseCoder struct {
-	alphabet string
-	base     int
-	length   int
-
-	charToIndex map[rune]int
+	alphabet        string
+	base            int
+	length          int
+	charToIndex     map[rune]int
+	maxDecodedValue uint64
 }
 
 var _ Coder = (*BaseCoder)(nil)
@@ -29,16 +30,23 @@ func NewBaseCoder(alphabet string, length int) (*BaseCoder, error) {
 		charToIndex[char] = i
 	}
 
+	maxDecodedValue := uint64(math.Pow(float64(len(alphabet)), float64(length))) - 1
+
 	return &BaseCoder{
-		alphabet:    alphabet,
-		base:        len(alphabet),
-		length:      length,
-		charToIndex: charToIndex,
+		alphabet:        alphabet,
+		base:            len(alphabet),
+		length:          length,
+		charToIndex:     charToIndex,
+		maxDecodedValue: maxDecodedValue,
 	}, nil
 }
 
 func (coder BaseCoder) Encode(n uint64) (string, error) {
 	var encoded strings.Builder
+
+	if n > coder.maxDecodedValue {
+		return "", errors.New("number is too large to be encoded")
+	}
 
 	for n > 0 {
 		rem := n % uint64(coder.base)
@@ -60,6 +68,10 @@ func (coder BaseCoder) Encode(n uint64) (string, error) {
 
 func (coder BaseCoder) Decode(s string) (uint64, error) {
 	var n uint64
+
+	if len(s) != coder.length {
+		return 0, errors.New("invalid encoded string length")
+	}
 
 	for _, char := range s {
 		index, exists := coder.charToIndex[char]
